73ea40b7a72f80288938c7e6a5238d93de4b0b71
Camryn Drake <cdrake@students.inghamisd.org>
2018-02-27 12:11:12 -0500

    practice for real thing

---
+++ SimpleGameEngine/app/src/main/java/com/example/cdrake/simplegameengine/SimpleGameEngine.java
@@ -0,0 +1,33 @@
+package com.example.cdrake.simplegameengine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SimpleGameEngine extends Activity {
+    GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gameView = new GameView(this);
+        setContentView(gameView);
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pause();
+    }
+}
+
+
+
+
+
+
+
